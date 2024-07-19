c60360d0bdf4d0859ae0b352f71311ee6f1bf482
Ankit Seth <cosmic.ankit@gmail.com>
2024-07-19 19:39:04 +0530

    request processing with multiplexer accepting limited request

--- goconcurrency.go
+++ goconcurrency.go
@@ -3,6 +3,7 @@ package main
 import (
 	"container/heap"
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -10,7 +11,7 @@ type Payload struct {
 	uuid string
 }
 type Request struct {
-	id       int
+	id       string
 	payLoad  Payload
 	arrival  time.Time
 	priority int
@@ -44,94 +45,142 @@ func (pq *PriorityQueue) Pop() interface{} {
 }
 
 type MultiPlexer struct {
-	pq PriorityQueue
-	//workerPool *WorkerPool
+	pq         PriorityQueue
+	wg         sync.WaitGroup
+	workerPool *WorkerPool
 }
 
-func NewMultiplexer() *MultiPlexer {
+func NewMultiplexer(numWorkers int) *MultiPlexer {
 	pq := make(PriorityQueue, 0)
 	heap.Init(&pq)
+	workerPool := NewWorkerPool(numWorkers)
 	return &MultiPlexer{
-		pq: pq,
-		//workerPool: workerPool,
+		pq:         pq,
+		workerPool: workerPool,
 	}
 }
 
-// func (m *MultiPlexer) scheduleNext() {
-// 	if len(m.pq) > 0 {
-// 		req := heap.Pop(&m.pq).(*Request)
-// 		m.workerPool.submitRequest(req)
-// 	}
-// }
-
-func (mux *MultiPlexer) handleRequest(inputChannel <-chan *Request, numRequest int) <-chan Request {
-	// Apply logic
-	// 1. add the request to priority que
-	// 2. submit request to worker pool to handle it
-	// 3. expect next request
-
-	processChannel := make(chan Request, numRequest)
-	go func() {
-		for req := range inputChannel {
-			fmt.Printf("req :%v pushed to heap \n", req.id)
-			req.priority = int(time.Since(req.arrival).Milliseconds())
-			heap.Push(&mux.pq, req)
-			processChannel <- *req
-		}
-		close(processChannel)
-	}()
-	return processChannel
+type WorkerPool struct {
+	workChan chan *Request
+	outChan  chan *Request
+	wg       sync.WaitGroup
 }
 
-func (mux *MultiPlexer) popFromHeap(inputChannel <-chan Request, numRequest int) <-chan Request {
-	processChannel := make(chan Request, numRequest)
-	go func() {
-		for req := range inputChannel {
-			fmt.Printf("channel id : %v is processing \n", req.id)
-			item := heap.Pop(&mux.pq).(*Request)
-			fmt.Printf("pop request: %v is processing \n", item.id)
-			processChannel <- *item
-		}
-		close(processChannel)
-	}()
-	return processChannel
+func NewWorkerPool(numWorkers int) *WorkerPool {
+	workChan := make(chan *Request, 10000000)
+	outChan := make(chan *Request, 10000000)
+	pool := &WorkerPool{workChan: workChan, outChan: outChan}
+	pool.startWorkers(numWorkers)
+	return pool
 }
 
-func (mux *MultiPlexer) ProcessRequest(inputChannel <-chan Request, numRequest int) <-chan Request {
-	processChannel := make(chan Request, numRequest)
-	go func() {
-		for req := range inputChannel {
-			time.Sleep(time.Second * 1)
-			fmt.Printf("finished processing request %d\n", req.id)
-			processChannel <- req
-		}
-		close(processChannel)
-	}()
-	return processChannel
+func (p *WorkerPool) startWorkers(numWorkers int) {
+	for i := 0; i < numWorkers; i++ {
+		p.wg.Add(1)
+		go func(id int) {
+			//defer close(p.outChan)
+			defer p.wg.Done()
+			for req := range p.workChan {
+				// Simulate work
+				time.Sleep(10 * time.Second) // Assume each request takes 10 seconds to process
+				//fmt.Printf("Worker %d finished processing request %s\n", id, req.payLoad.uuid)
+				p.outChan <- req
+			}
+		}(i)
+
+	}
+	// for i := 0; i < numCPU; i++ {
+	//     <-c    // wait for one task to complete
+	// }
 }
 
-func main() {
-	totalRequest := 9
-	inputChannel := make(chan *Request, totalRequest)
-	mux := NewMultiplexer()
+func (p *WorkerPool) submitRequest(req *Request) {
+	p.workChan <- req
+}
+
+func (m *MultiPlexer) AddRequest(req *Request) {
+	//m.mu.Lock()
+	//defer m.mu.Unlock()
+	req.priority = int(time.Since(req.arrival).Milliseconds())
+
+	heap.Push(&m.pq, req)
+	m.scheduleNext()
+}
+
+func (m *MultiPlexer) scheduleNext() {
+	if len(m.pq) > 0 {
+		req := heap.Pop(&m.pq).(*Request)
+		m.workerPool.submitRequest(req)
+	}
+}
+
+func (p *WorkerPool) shutdown() {
+	close(p.workChan)
+	close(p.outChan)
+	p.wg.Wait()
+}
+
+func simulateRequests(numUsers int, requestsPerUser int, inputChannel chan *Request) {
 	go func() {
-		for i := 0; i < 10; i++ {
-			inputChannel <- &Request{
-				id:      i,
-				arrival: time.Now(),
-				payLoad: Payload{uuid: fmt.Sprintf("uuid-%d", i)},
+		for j := 0; j < numUsers; j++ {
+			for i := 0; i < requestsPerUser; i++ {
+				inputChannel <- &Request{
+					id:      fmt.Sprintf("id-%v-%v", j, i),
+					arrival: time.Now(),
+					payLoad: Payload{uuid: fmt.Sprintf("uuid-%d-%d", j, i)},
+				}
 			}
 		}
 		close(inputChannel)
 	}()
+}
+
+// func (m *MultiPlexer)AddRequestToPriorityQue(){
+
+// }
+
+func main() {
+	numUsers := 20
+	requestsPerUser := 1200
+	totalRequests := numUsers * requestsPerUser
+	numWorkers := 18000 / 6
+	inputChannel := make(chan *Request, totalRequests)
+	mux := NewMultiplexer(numWorkers)
+	simulateRequests(numUsers, requestsPerUser, inputChannel)
 	start := time.Now()
-	handleRequestToMultiplexChannel := mux.handleRequest(inputChannel, totalRequest)
-	poppedRequestFromPQChannel := mux.popFromHeap(handleRequestToMultiplexChannel, totalRequest)
-	processedRequestChannel := mux.ProcessRequest(poppedRequestFromPQChannel, totalRequest)
-	for n := range processedRequestChannel {
-		fmt.Printf("request : %v is processed \n", n.id)
-	}
+	processed := totalRequests
+	go func() {
+		ticker := time.NewTicker(time.Minute)
+		defer ticker.Stop()
+		batchSize := 18000
+		requestsProcessed := 0
+		for req := range inputChannel {
+			if requestsProcessed < batchSize {
+				mux.AddRequest(req)
+				requestsProcessed++
+			} else {
+				mux.AddRequest(req)
+				processed = processed - requestsProcessed
+				fmt.Printf("queued : %v \n", processed)
+				fmt.Printf("processed : %v \n", requestsProcessed)
+				<-ticker.C
+				requestsProcessed = 1
+			}
+		}
+		if requestsProcessed > 0 {
+			processed = processed - requestsProcessed
+			fmt.Printf("queued : %v \n", processed)
+			fmt.Printf("processed : %v \n", requestsProcessed)
+		}
+
+	}()
 	fmt.Println("channel closed")
+	time.Sleep(2 * time.Minute)
+	mux.workerPool.shutdown()
+	if mux.pq.Len() == 0 {
+		fmt.Println("all request processed")
+	}
+	fmt.Printf("heap length :  %v \n", mux.pq.Len())
 	duration := time.Since(start)
 	fmt.Printf("Total time taken: %s\n", duration)
 }
